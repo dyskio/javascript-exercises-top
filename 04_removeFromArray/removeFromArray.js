73cca9d4553a93af6cb24936415963e1af869858
dyskio <130579182+dyskio@users.noreply.github.com>
2023-09-14 18:58:04 +0200

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    let newArray = [];
+    for (let i = 0; i < remove.length; i++){
+        if (arr.includes(remove[i])){
+            let ele = remove[i];
+            let index = arr.indexOf(ele);
+            arr.splice(index, 1);
+        } else {
+            continue
+        }
+    }
+    return arr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// expect(removeFromArray([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
