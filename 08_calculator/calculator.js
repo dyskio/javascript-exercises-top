938d0ecda55bdecc8dbaea6806a786c602584577
dyskio <130579182+dyskio@users.noreply.github.com>
2023-09-15 22:11:34 +0200

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let total = 0;
+  for (let num of args) {
+      if (Array.isArray(num)) {
+          total += sum(...num); // Recursively call sum for nested arrays
+      } else {
+          total += num;
+      }
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  const flattenedArray = args.flat(Infinity);
+  if (flattenedArray.length === 0) {
+      return 0;
+  }
+  return flattenedArray.reduce((acc, num) => acc * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	if (num === 0 || num === 1){
+    return 1;
+  } else {
+    for (let i = 1; i <= num; i++){
+      total *= i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
