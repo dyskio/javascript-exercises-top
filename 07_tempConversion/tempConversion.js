5a8dc52f505f93151c8fce8abdc3c22ed83b895e
dyskio <130579182+dyskio@users.noreply.github.com>
2023-09-15 09:56:09 +0200

    completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  // The formula for converting Fahrenheit to Celsius is C = 5/9(F-32).
+  let result = (5/9) * (fah - 32);
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  // The formula for converting Celsius to Fahrenheit is F = (C × 9/5) + 32
+  let result = (cel * (9/5) + 32);
+  result = Math.round(result * 10) / 10;
+  return result;
+
 };
 
 // Do not edit below this line
