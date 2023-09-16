1e3042c15d7823608df51334e2ab3feda4f4edd3
dyskio <130579182+dyskio@users.noreply.github.com>
2023-09-16 11:38:14 +0200

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (inp) {
+    // regex unicode punctuation
+    const punctuation = /[\p{P}\p{S}]/gu;
+    // regex whitespace
+    let whitespace = /\s/g;
 
+    // remove whitespace & punctuation
+    let clean = inp.replace(whitespace, "");
+    clean = clean.replace(punctuation, "");
+
+    // 
+    let reversed = clean.split("").reverse().join("");
+    
+    if (reversed.toLowerCase() === clean.toLowerCase()){
+        return true;
+    } else {
+        return false;
+    }
+
+    
 };
 
 // Do not edit below this line
