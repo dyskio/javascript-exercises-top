2182ba8e5a6f9f735f169995e9d20c1389de6671
dyskio <130579182+dyskio@users.noreply.github.com>
2023-09-16 13:29:16 +0200

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS"
+    }
+    let seq = [0, 1];
+    for (let i=2; i < num; i++){
+        seq[i] = seq[i-1] + seq[i-2]
+    }
+    let total = seq[seq.length - 1] + seq[seq.length - 2];
+    return total;
 };
 
 // Do not edit below this line
